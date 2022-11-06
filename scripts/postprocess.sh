ded2acc8132e36a1c56d6b880defa55cb000dcbd
David Buch <davidbuch42@gmail.com>
2022-11-06 11:39:56 -0500

    moving a script

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#SBATCH -o ./output/us_analysis/logs/results_%a.txt                                                              
-#SBATCH -e ./output/us_analysis/logs/errors_%a.err                                                                     
+#SBATCH -o ./output/us_analysis/logs/postprocess_results.txt                                                              
+#SBATCH -e ./output/us_analysis/logs/postprocess_errors.err                                                                     
 #SBATCH -p dunsonlab --account=dunsonlab                                                                                
 #SBATCH --mem=10G
-#SBATCH -a 1-81
 
 module load R
-Rscript ./scripts/analysis_scripts/us_analysis_master_script.R
+Rscript ./scripts/analysis_scripts/sim_study_meta_postprocess.R
+Rscript ./scripts/analysis_scripts/rolling_qdl_comparison.R
+Rscript ./scripts/analysis_scripts/us_analysis_nonparametric.R
+Rscript ./scripts/analysis_scripts/us_analysis_postprocessing_endpoints.R
+Rscript ./scripts/analysis_scripts/us_analysis_postprocessing_sensitivity.R
 module unload R
